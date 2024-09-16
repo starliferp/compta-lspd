6351207dc393ad5b454d6f62a747f31902b99215
starliferp <nathancourstle@gmail.com>
2024-09-16 12:38:48 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const WEBHOOK_URL = 'https://discord.com/api/webhooks/1285169873672077373/AU3rx6_GQA8aNjUyZouNG1UtA8WUCVQps58cTFQfqNoKHFPjoJU5YT7yttxhRB8OrMFw';
+const WEBHOOK_URL = 'https://discord.com/api/webhooks/1285188019103469609/o-2xPi7VaO1IUEuKsPwHjMJ_OLyNIBIVLuCM5xavUDbLp9zgDwMhNukK19O-Oge7dvAF';
 export { WEBHOOK_URL };
